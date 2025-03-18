cebc30759f95163cdfba25d4273366660cad7c9a
Cenarius <kiss.2002e@gmail.com>
2025-03-18 09:52:41 +0100

    Revert earlier changes

---
+++ recipes/default/example/examples/index.php
@@ -0,0 +1,9 @@
+Examples:
+<div>
+    <li><a href="/database_example.php">Database connection</a></li>
+    <li><a href="/email_example.php">Email sending (run composer install first)</a></li>
+</div>
+
+<?php
+phpinfo();
+?>
